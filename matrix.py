daad518a4c6fa8b36064ac0daffcb576f10f28a8
Sailesh Simhadri <sailesh@localhost.localdomain>
2016-10-06 22:41:42 -0400

    Implement scalar multiplication.

--- matrix.py
+++ matrix.py
@@ -39,8 +39,14 @@ class Matrix:
 
     def __mul__(self, other):
         """Multiply this matrix with another matrix"""
-        return Matrix(*[Vector(*i) for i in
+        if isinstance(other, (int, float)):
+            return Matrix(*[Vector(*i) for i in
+                            np.multiply(self._columns, other)])
+        elif type(self) == type(other):
+            return Matrix(*[Vector(*i) for i in
                         self._columns.dot(other.to_array()).T])
+
+    __rmul__ = __mul__
 
     def det(self):
         """Return determinant of matrix"""
