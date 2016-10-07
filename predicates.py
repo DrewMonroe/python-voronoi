dcc94fc9f1a97e7b06cb40597fcc63fe00c34dd1
josephsweeney <joseph.sweeney95@gmail.com>
2016-10-06 23:23:59 -0400

    removed unecessary code

--- predicates.py
+++ predicates.py
@@ -13,55 +13,6 @@ from matrix import Matrix
 from vector import Vector
 
 
-def vector(*values):
-    """Make a column vector (secretly a numpy array)"""
-    return np.array([[v] for v in values])
-
-
-def matrix(*rows):
-    """Make a matrix with the given rows."""
-    return np.array(rows)
-
-
-def equal(matrix0, matrix1):
-    """Test matrices/vectors for equality.
-
-    This matters because numpy doesn't let you use == because of
-    ambiguity.
-    """
-    return (matrix0.shape == matrix1.shape and
-            np.equal(matrix0, matrix1).all())
-
-
-def as_columns(*vectors):
-    """Return a matrix with those vectors as columns.
-
-    Non-distructive - copies everything.
-    """
-    return np.concatenate(vectors, axis=1)
-
-
-def lift(vect, function=(lambda *args: 1)):
-    """Takes a column vector vect (np.ndarray) and lifts it using function.
-
-    Check out this sweet syntax!
-    """
-    return np.append(vect, np.array([[function(vect)]]),
-                     axis=0)  # 8.7s
-
-
-def sign_det(mtrx):
-    """Return just the sign (-1, 0, 1) of the determinant of matrix mtrx.
-
-    Hopefully this can be optimized to be faster than a full computation
-    of the determinant. (TODO)
-    https://math.stackexchange.com/questions/693948/simple-
-    way-to-find-the-sign-of-a-determinant-given-a-singular-
-    value-decompositio
-    """
-    return linalg.slogdet(mtrx)[0]
-
-
 def ccw(*vectors):
     """tests if triangle a, b, c is oriented counterclockwise.
 
@@ -73,12 +24,6 @@ def ccw(*vectors):
     return int(np.sign(m.det()))
 
 
-def norm_squared(column):
-    """The dot-product norm on R^d. Used by incircle."""
-    # This one might not be good/useful, but is handy for reference.
-    return column.T.dot(column)[0][0]
-
-
 def incircle(*vectors):  # a, b, c, d):
     """In R^2, is the last argument inside the circle defined by the
     previous arguments?
@@ -87,12 +32,3 @@ def incircle(*vectors):  # a, b, c, d):
 
     return (ccw(*[lift(v, norm_squared) for v in vectors]) *
             ccw(*vectors[:-1]))  # eliminate dependence on orienation of circle
-
-
-def lift_matrix(base_matrix, last_row):
-    """Append a given row to the bottom of a matrix.
-
-    The row will be flattened, which is convenient only when you weren't
-    making a 3-D matrix. (I hope it never comes to that anyway.)
-    """
-    return np.append(base_matrix, last_row, axis=0)  # fails the test
