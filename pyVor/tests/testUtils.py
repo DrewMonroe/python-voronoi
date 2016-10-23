90ab77b867c72e65195aec4f8f7df4b8642f7f28
josephsweeney <joseph.sweeney95@gmail.com>
2016-10-23 17:51:43 -0400

    fixes circumcenter, adds unit tests

--- pyVor/tests/testUtils.py
+++ pyVor/tests/testUtils.py
@@ -10,13 +10,22 @@ class UtilsTestCase(unittest.TestCase):
 
     def testCircumCenter(self):
         """ Tests for corect circumcenter """
-        a = Point(0, 1)
-        b = Point(1, 0)
+        a = Point(1, 0)
+        b = Point(0, 1)
         c = Point(0, -1)
         zero = Point(0.0, 0.0)
-        center = circumcenter(a, b, c)
-        self.assertTrue(type(center) is Point)
-        self.assertTrue(center == zero)
+        center1 = circumcenter(a, b, c)
+        self.assertTrue(type(center1) is Point)
+        self.assertTrue(center1 == zero)
+        d = Point(0, 0, 1)
+        center2 = circumcenter(a.lift(lambda v: 0),
+                               b.lift(lambda v: 0), c.lift(lambda v: 0), d)
+        self.assertTrue(center2 == zero.lift(lambda v: 0.0))
+        a = Point(1, 2)
+        b = Point(2, 1)
+        c = Point(1, 0)
+        center3 = circumcenter(a, b, c)
+        self.assertTrue(center3 == Point(1.0, 1.0))
 
 if __name__ == "__main__":
     unittest.main()
--- pyVor/utils.py
+++ pyVor/utils.py
@@ -2,9 +2,8 @@ from pyVor.primitives import Point, Vector, Matrix
 
 
 def circumcenter(*points):
-    vectors = [p-points[0] for p in points]
-    del vectors[0]
-    A = Matrix(*vectors)
+    vectors = [p-points[0] for p in points[1:]]
+    A = Matrix(*vectors).transpose()
     p0Squared = points[0].to_vector().norm_squared()
     b = Vector(*[0.5*(p.to_vector().norm_squared()-p0Squared)
                  for p in points[1:]])
